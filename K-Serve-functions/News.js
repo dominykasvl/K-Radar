76cf33b61a2bd215a5771cd6636a809140206b42
Dominykas <48452225+dominykasvl@users.noreply.github.com>
2024-05-05 12:05:36 +0000

    News function rewritten in axios and cheerio

--- K-Serve-functions/News.js
+++ K-Serve-functions/News.js
@@ -1,108 +1,44 @@
-const htmlparser2 = require("htmlparser2-without-node-native");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 async function fetchNewsData() {
   const url = "https://www.allkpop.com";
 
   try {
     console.log("Fetching data...");
-    const response = await fetch(url);
-    const content = await response.text();
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const articlesData = [];
 
-    return new Promise((resolve, reject) => {
-      const html = content;
-      const handler = new htmlparser2.DomHandler(async (error, dom) => {
-        if (error) {
-          throw new Error("An error occurred while parsing HTML.");
-        } else {
-          const articles = htmlparser2.DomUtils.findAll(
-            (el) =>
-              el.attribs &&
-              el.attribs.class &&
-              el.attribs.class.split(" ").includes("list"),
-            dom,
-          );
-          const articlesData = articles
-            ? articles
-                .map((article) => {
-                  //return title and link
-                  const titleDivs = htmlparser2.DomUtils.findAll(
-                    (el) =>
-                      el.attribs &&
-                      el.attribs.class &&
-                      el.attribs.class.split(" ").includes("title"),
-                    article.children,
-                  );
-                  if (titleDivs.length !== 1) return null;
-                  const titleElement = htmlparser2.DomUtils.findOne(
-                    (el) => el.name === "a",
-                    titleDivs[0].children,
-                  );
-                  const title = titleElement
-                    ? htmlparser2.DomUtils.getText(titleElement)
-                    : null;
-                  const link = titleElement
-                    ? url +
-                      htmlparser2.DomUtils.getAttributeValue(
-                        titleElement,
-                        "href",
-                      )
-                    : null;
+    $(".list").each(function () {
+      const titleElement = $(this).find(".title a");
+      const title = titleElement.text().trim();
+      const link = titleElement.attr("href")
+        ? url + titleElement.attr("href")
+        : null;
 
-                  //return image
-                  const imageDivs = htmlparser2.DomUtils.findAll(
-                    (el) =>
-                      el.attribs &&
-                      el.attribs.class &&
-                      el.attribs.class.split(" ").includes("image"),
-                    article.children,
-                  );
-                  if (imageDivs.length !== 1) return null;
-                  const imageElement = htmlparser2.DomUtils.findOne(
-                    (el) => el.name === "img",
-                    imageDivs[0].children,
-                  );
-                  const image = imageElement
-                    ? htmlparser2.DomUtils.getAttributeValue(
-                        imageElement,
-                        "data-src",
-                      )
-                    : null;
+      const imageElement = $(this).find(".image img");
+      const image = imageElement.attr("data-src")
+        ? imageElement.attr("data-src")
+        : null;
 
-                  //return unix timestamp
-                  const timeElement = htmlparser2.DomUtils.findAll(
-                    (el) =>
-                      el.attribs &&
-                      el.attribs.class &&
-                      el.attribs.class.split(" ").includes("realtime"),
-                    article.children,
-                  );
-                  if (timeElement.length !== 1) return null;
-                  const timestamp = timeElement
-                    ? htmlparser2.DomUtils.getAttributeValue(
-                        timeElement[0],
-                        "data-ts",
-                      )
-                    : null;
+      const timeElement = $(this).find(".realtime");
+      const timestamp = timeElement.attr("data-ts")
+        ? timeElement.attr("data-ts")
+        : null;
 
-                  //return empty summary
-                  const summary = "";
+      const summary = ""; // Assuming summary is empty as per original logic
 
-                  if (!title && !image && !timestamp && !link) return null;
-
-                  return { title, image, timestamp, link, summary };
-                })
-                .filter(Boolean)
-            : [];
-          resolve(articlesData);
-        }
-      });
-
-      const parser = new htmlparser2.Parser(handler);
-      parser.write(html);
-      parser.end();
+      // Only add to array if all necessary data is present
+      if (title && link && image && timestamp) {
+        articlesData.push({ title, link, image, timestamp, summary });
+      }
     });
+
+    return articlesData;
   } catch (error) {
     console.error(`An error occurred: ${error.message}`);
+    return []; // Return an empty array in case of error
   }
 }
 
